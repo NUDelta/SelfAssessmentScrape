ad0a3a8f4b5efe21d66a787ee942b1471c3d4d06
noradiegwu <nneomaoradiegwu2019@u.northwestern.edu>
2018-01-10 20:02:54 -0600

    free responses imported in output sheet

--- Self-Assessment_Scrape.py
+++ Self-Assessment_Scrape.py
@@ -9,44 +9,65 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('IDP_keys.json', 
 gc = gspread.authorize(credentials)
 
 
-# open worksheet
+# open self-assessment worksheet
 spreadsheet = gc.open_by_key('1a4-t-LI_IwfcM02byKARwBcU5-nIEMzFy_ohy_rtp1Q')
 # won't open...
     # resolution!: go to spreadsheet and share sheet w/ client_email in .json keys file
 
 # get prompts and responses to design, technology, and research sheets
 def DTRFreeResponses():
+    prompt_list =[]
+    response_list =[]
     for page_number in range(3, 6):
         sheet = spreadsheet.get_worksheet(page_number)
         for row in range(3, 6):
             prompt = sheet.acell('A%d' % row).value
             response = sheet.acell('B%d' % row).value
-            print prompt + ': ' + response
-            print
-        print
+            # print prompt + ': ' + response
+            prompt_list.append(prompt)
+            response_list.append(response)
+    result = {'prompts': prompt_list, 'responses': response_list}
+    return result
 
 def LIPFreeResponse():
+    prompt_list =[]
+    response_list =[]
     sheet = spreadsheet.worksheet('LIP')
     for row in range(5, 8):
         prompt = sheet.acell('A%d' % row).value
         response = sheet.acell('B%d' % row).value
-        print prompt + ': ' + response
-        print
+        # print prompt + ': ' + response
+        prompt_list.append(prompt)
+        response_list.append(response)
+    result = {'prompts': prompt_list, 'responses': response_list}
+    return result
+
 
 def CollaborationFreeResponse():
+    prompt_list =[]
+    response_list =[]
     sheet = spreadsheet.worksheet('Collaboration')
     for row in range(4, 7):
         prompt = sheet.acell('A%d' % row).value
         response = sheet.acell('B%d' % row).value
-        print prompt + ': ' + response
-        print
+        # print prompt + ': ' + response
+        prompt_list.append(prompt)
+        response_list.append(response)
+    result = {'prompts': prompt_list, 'responses': response_list}
+    return result
 
 def GrowthFreeResponse():
+    prompt_list =[]
+    response_list =[]
     sheet = spreadsheet.worksheet('Growth')
     for row in range(5, 8):
         prompt = sheet.acell('A%d' % row).value
         response = sheet.acell('B%d' % row).value
-        print prompt + ': ' + response
+        # print prompt + ': ' + response
+        prompt_list.append(prompt)
+        response_list.append(response)
+    result = {'prompts': prompt_list, 'responses': response_list}
+    return result
 
 
 def DTRProcess():
@@ -83,11 +104,29 @@ def ResearchProcess():
                 not_agree_count += 1
     print not_agree_count
 
+# output worksheet
+output_spreadsheet = gc.open_by_key('1qTcgo015pJfn-w-r4SyvrOYmQy3HC_XzzQy1TISsgqk')
+
 def FreeResponses():
-    DTRFreeResponses()
-    LIPFreeResponse()
-    CollaborationFreeResponse()
-    GrowthFreeResponse()
+    dtr_dict = DTRFreeResponses()
+    lip_dict = LIPFreeResponse()
+    collab_dict = CollaborationFreeResponse()
+    growth_dict = GrowthFreeResponse()
+
+    free_response_prompts = dtr_dict['prompts'] + lip_dict['prompts'] + collab_dict['prompts'] + growth_dict['prompts']
+    free_response_responses = dtr_dict['responses'] + lip_dict['responses'] + collab_dict['responses'] + growth_dict['responses']
+
+    wks = output_spreadsheet.get_worksheet(0)
+    cell_list_a = wks.range('A2:A18')
+    cell_list_b = wks.range('B2:B18')
+
+    for cell in range(0, 17):
+        cell_list_a[cell].value = free_response_prompts[cell]
+        cell_list_b[cell].value = free_response_responses[cell]
+    wks.update_cells(cell_list_a)
+    wks.update_cells(cell_list_b)
+
+FreeResponses()
 
 def Processes():
     DTRProcess()
@@ -97,9 +136,25 @@ def AnalyzeSelfAssessment():
     FreeResponses()
     Processes()
 
-AnalyzeSelfAssessment()
+
+
+# AnalyzeSelfAssessment()
+
+
+# putting the data into a spreadsheet
+
+# # # Update cell range
+# wks = output_spreadsheet.get_worksheet(0)
+# cell_list = wks.range('A1:A17')
+# for cell in cell_list:
+#     cell.value = 'dfjkasd'
+#
+# wks.update_cells(cell_list) # Update in batch
+
+
 
 # next steps:
     # put the output in a format that'll be easy for people to read
     # collect data on highest instances of "not agreeing"
     # run for each self-assessment
+    # make faster if time
